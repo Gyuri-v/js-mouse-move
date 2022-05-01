bcdd7a003f7dfddde617215bac25324b99369e4c
GYURI <fnffkdll@naver.com>
2022-05-01 23:20:21 +0900

    mouse animation 오류 수정 - 마우스 y 포지션 따로오도록

--- main.js
+++ main.js
@@ -15,14 +15,14 @@ const setPostion = function (e) {
   cursor.style.top = delta.y - cursor.offsetHeight / 2 + 'px';
   cursor.style.left = delta.x - cursor.offsetWidth / 2 + 'px';
 
-  images[1].style.transform = `translate3d(${position.x * -0.03}px, ${position.x * -0.03}px, 0)`;
-  images[2].style.transform = `translate3d(${position.x * -0.02}px, ${position.x * -0.02}px, 0)`;
-  images[3].style.transform = `translate3d(${position.x * -0.03}px, ${position.x * -0.03}px, 0)`;
-  images[4].style.transform = `translate3d(${position.x * -0.04}px, ${position.x * -0.04}px, 0)`;
-  images[5].style.transform = `translate3d(${position.x * -0.05}px, ${position.x * -0.05}px, 0)`;
-  images[6].style.transform = `translate3d(${position.x * -0.02}px, ${position.x * -0.02}px, 0)`;
-  images[7].style.transform = `translate3d(${position.x * -0.01}px, ${position.x * -0.01}px, 0)`;
-  images[8].style.transform = `translate3d(${position.x * -0.01}px, ${position.x * -0.01}px, 0)`;
+  images[1].style.transform = `translate3d(${position.x * -0.03}px, ${position.y * -0.03}px, 0)`;
+  images[2].style.transform = `translate3d(${position.x * -0.02}px, ${position.y * -0.02}px, 0)`;
+  images[3].style.transform = `translate3d(${position.x * -0.03}px, ${position.y * -0.03}px, 0)`;
+  images[4].style.transform = `translate3d(${position.x * -0.04}px, ${position.y * -0.04}px, 0)`;
+  images[5].style.transform = `translate3d(${position.x * -0.05}px, ${position.y * -0.05}px, 0)`;
+  images[6].style.transform = `translate3d(${position.x * -0.02}px, ${position.y * -0.02}px, 0)`;
+  images[7].style.transform = `translate3d(${position.x * -0.01}px, ${position.y * -0.01}px, 0)`;
+  images[8].style.transform = `translate3d(${position.x * -0.01}px, ${position.y * -0.01}px, 0)`;
 };
 
 window.addEventListener('mousemove', setPostion);
