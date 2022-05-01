cc09e5bab750ff93bdf9f5498ccd29d4f69e673f
GYURI <fnffkdll@naver.com>
2022-05-01 23:19:45 +0900

    cursor 추가

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+const cursor = document.querySelector('.cursor');
 const images = document.querySelectorAll('.imgs__img');
 
-const easeFactor = 0.3;
+const easeFactor = 0.15;
 let position = { x: 0, y: 0 };
 let delta = { x: 0, y: 0 };
 
 const setPostion = function (e) {
   position.x = e.clientX;
   position.y = e.clientY;
+
+  delta.x += (position.x - delta.x) * easeFactor;
+  delta.y += (position.y - delta.y) * easeFactor;
+
+  cursor.style.top = delta.y - cursor.offsetHeight / 2 + 'px';
+  cursor.style.left = delta.x - cursor.offsetWidth / 2 + 'px';
 
   images[1].style.transform = `translate3d(${position.x * -0.03}px, ${position.x * -0.03}px, 0)`;
   images[2].style.transform = `translate3d(${position.x * -0.02}px, ${position.x * -0.02}px, 0)`;
