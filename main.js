2ef14c2114748b79245dec054950151d6cd7f6e5
GYURI <fnffkdll@naver.com>
2022-05-01 23:22:18 +0900

    mouse animation - 돌은 안움직이도록 수정

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ const setPostion = function (e) {
   cursor.style.top = delta.y - cursor.offsetHeight / 2 + 'px';
   cursor.style.left = delta.x - cursor.offsetWidth / 2 + 'px';
 
-  images[1].style.transform = `translate3d(${position.x * -0.03}px, ${position.y * -0.03}px, 0)`;
   images[2].style.transform = `translate3d(${position.x * -0.02}px, ${position.y * -0.02}px, 0)`;
   images[3].style.transform = `translate3d(${position.x * -0.03}px, ${position.y * -0.03}px, 0)`;
   images[4].style.transform = `translate3d(${position.x * -0.04}px, ${position.y * -0.04}px, 0)`;
